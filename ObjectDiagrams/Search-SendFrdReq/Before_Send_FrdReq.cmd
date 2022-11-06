f42db2a82fa548e7b0a704f555c1c87e7a6d62a3
Patrick Witter <pwitterop123@gmail.com>
2022-11-06 18:49:44 -0500

    Added ObjDg for Send Friend Request UseCase

---
+++ ObjectDiagrams/Search-SendFrdReq/Before_Send_FrdReq.cmd
@@ -0,0 +1,14 @@
+
+
+reset
+
+--Create Objects
+
+!create player: Player 
+!create searchedPlayer: Player
+!create bssn: BattleShipSocialNetwork
+
+-- Create Assocation
+!insert (searchedPlayer,player) into SearchFor
+!insert (bssn,searchedPlayer) into Players
+!insert (bssn,player) into Players
