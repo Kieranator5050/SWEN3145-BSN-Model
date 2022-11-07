57f4e50ebb8540278ff36876adad2fd0cbe379e6
Patrick Witter <pwitterop123@gmail.com>
2022-11-06 21:22:57 -0500

    View LeaderBoard ObjDiagram and unfinished blocked player

---
+++ ObjectDiagrams/ViewLeaderBoard/After_ViewLeaderboard.cmd
@@ -0,0 +1,37 @@
+-- open ObjectDiagrams\SignUp\After_SignUp.cmd
+
+reset
+
+-- Create ObjectDiagrams
+
+!create p0: Player
+!create bssn: BattleShipSocialNetwork
+!create stats0: Stat
+!create stats1: Stat
+!create stats2: Stat
+!create leaderb: RegularLeaderboard
+
+
+-- Initializing Objects 
+
+-- stats0 
+!set stats0.name := PlayerStat::regGameWinLossRatio
+
+
+-- stats1
+!set stats1.name := PlayerStat::tournGameWinLossRatio
+
+
+--stats2
+!set stats2.name := PlayerStat::friendCount
+
+
+-- Create Assocation - PlayerStats
+
+!insert (p0,stats0) into PlayerStats
+!insert (p0,stats1) into PlayerStats
+!insert (p0,stats2) into PlayerStats
+!insert(bssn,leaderb) into RegLeaderboard
+-- Create Assocation - Players
+
+!insert (bssn,p0) into Players
