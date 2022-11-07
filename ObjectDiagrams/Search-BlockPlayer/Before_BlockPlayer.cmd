a0639ff43c28c40320583496ddeafe54ba0b61d5
Patrick Witter <pwitterop123@gmail.com>
2022-11-07 11:06:52 -0500

    Created ObjDg for block-player usecase

--- ObjectDiagrams/Search-BlockPlayer/Before_BlockPlayer.cmd
+++ ObjectDiagrams/Search-BlockPlayer/Before_BlockPlayer.cmd
@@ -1,2 +1,15 @@
-reset 
+-- open ObjectDiagrams\Search\After_Search.cmd
+
+reset
+
+--Create Objects
+
+!create player: Player 
+!create searchedPlayer: Player
+!create bssn: BattleShipSocialNetwork
+
+-- Create Assocation
+!insert (searchedPlayer,player) into SearchFor
+!insert (bssn,searchedPlayer) into Players
+!insert (bssn,player) into Players
 
